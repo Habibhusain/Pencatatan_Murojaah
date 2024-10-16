cf06d33e64e289f509e3a42b651128a41f65296e
Habib Husain Nurrohim <habibsain55@gmail.com>
2024-10-16 12:00:26 +0700

    update tambah

--- tambah_murojaah.php
+++ tambah_murojaah.php
@@ -49,36 +49,10 @@ if(isset($_POST['surah']) && $_POST['surah'] !=''){
                     <input type="date" name="tanggal" required>
                     <label for="">Juz</label>
                     <select name="juz" required>
-                        <option value="1">1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                        <option>6</option>
-                        <option>7</option>
-                        <option>8</option>
-                        <option>9</option>
-                        <option>10</option>
-                        <option>11</option>
-                        <option>12</option>
-                        <option>13</option>
-                        <option>14</option>
-                        <option>15</option>
-                        <option>16</option>
-                        <option>17</option>
-                        <option>18</option>
-                        <option>19</option>
-                        <option>20</option>
-                        <option>21</option>
-                        <option>22</option>
-                        <option>23</option>
-                        <option>24</option>
-                        <option>25</option>
-                        <option>26</option>
-                        <option>27</option>
-                        <option>28</option>
-                        <option>29</option>
-                        <option>30</option>
+                    <option value="1">1</option>
+                        <?php for ($i = 2; $i <= 30; $i++): ?>
+                            <option value="<?php echo $i; ?>"><?php echo $i; ?></option>
+                        <?php endfor; ?>
                     </select>
                     <label for="">Surah</label>
                     <input type="text" name="surah" required>
